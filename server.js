13e106518523b15cb4a1f3810fde58d25694e588
mesadhan <sadhansarker3@gmail.com>
2019-07-29 14:27:39 +0600

    make some optimization and change port

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.use(express.static('public', {
 }));
 
 // Start server
-app.listen(4200, () => console.log('Server running on :- http://localhost:4200'));
+app.listen(4100, () => console.log('Server running on :- http://localhost:4100'));
